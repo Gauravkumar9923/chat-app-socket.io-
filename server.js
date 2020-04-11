90c8946c762fc50f53686470e14d5650c4bea261
Gauravkumar9923 <43923144+Gauravkumar9923@users.noreply.github.com>
2020-04-11 11:11:36 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const path = require('path')
+const socketio = require('socket.io')
+const http = require('http')
+
+const app = express();
+const server = http.createServer(app)
+const io = socketio(server)
+
+let usersockets = {}
+
+app.use('/', express.static(path.join(__dirname, '/se')))
+
+io.on('connection', (socket) => {
+    console.log("New socket formed from " + socket.id)
+    socket.emit('connected')
+
+    socket.on('login', (data) => {
+        // username is in data.user
+        usersockets[data.user] = socket.id
+        console.log(usersockets)
+    })
+    
+    socket.on('send_msg', (data) => {
+    
+        if (data.message.startsWith('@')) {
+        
+            
+        let receiver = data.message.split(':')[0].substr(1)
+            let recieveSocket = usersockets[receiver]
+            io.to(recieveSocket).emit('recv_msg', data)
+        } else {
+            socket.broadcast.emit('recv_msg', data)            
+        }
+    })
+
+})
+
+server.listen(2345, () => console.log('Website open on http://localhost:2345'))
